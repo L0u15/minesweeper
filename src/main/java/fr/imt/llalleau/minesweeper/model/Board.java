625a2c59a5346f58f14d840eabf5fbffae7965c3
A678142 <A678142@EFR03403.fr01.awl.atosorigin.net>
2018-03-14 16:20:30 +0100

    addNumber

--- src/main/java/fr/imt/llalleau/minesweeper/model/Board.java
+++ src/main/java/fr/imt/llalleau/minesweeper/model/Board.java
@@ -2,8 +2,10 @@ package fr.imt.llalleau.minesweeper.model;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import fr.imt.llalleau.minesweeper.model.square.Mine;
 import fr.imt.llalleau.minesweeper.model.square.Number;
@@ -34,8 +36,7 @@ public class Board {
 		this.minePosition = new ArrayList<Point>();
 
 		this.placeMine(nb_mine);
-		this.placeZero();
-		this.placeNumber();
+		this.placeNumberRecursive(new Point(this.width, this.height));
 
 	}
 
@@ -55,20 +56,91 @@ public class Board {
 		} while (nb_mine > 0);
 	}
 
-	private void placeNumber() {
-		// TODO Auto-generated method stub
+	private void placeNumberRecursive(Point point) {
+		if (this.pointIsOutOfBounds(point)) {
+			return;
+		}
+		List<Point> pointAround = this.getPointAround(point);
+		List<Square> SquareAround = this.getSquaresFromPoint(pointAround);
+		if (this.getSquare(point) == null) {
+			int nbMineAround = this.countMine(SquareAround);
+			this.setSquare(point, new Number(nbMineAround));
+		}
+		for (Point newtPoint : pointAround) {
+			placeNumberRecursive(newtPoint);
+		}
 
 	}
 
-	public void placeZero() {
-		for (int h = 0; h < this.height; h++) {
-			for (int w = 0; w < this.width; w++) {
-				Square square = this.tab[h][w];
-				if (square == null) {
-					this.tab[h][w] = new Number();
-				}
+	private int countMine(List<Square> squareAround) {
+		int count = 0;
+		for (Square square : squareAround) {
+			if (square instanceof Mine)
+				count++;
+		}
+		return count;
+	}
+
+	private boolean pointIsOutOfBounds(Point point) {
+		return point.getY() < 0 || point.getY() >= this.height || point.getX() < 0 || point.getX() >= this.width;
+	}
+
+	private int countMineAround(Point point) {
+		int count = 0;
+		List<Square> squareAround = this.getSquareAround(point);
+		for (Square square : squareAround) {
+			if (square instanceof Mine)
+				count++;
+		}
+		return count;
+	}
+
+	private List<Square> getSquareAround(Point centralPoint) {
+
+		List<Square> squareAround = new ArrayList<>();
+
+		List<Point> pointAround = this.getPointAround(centralPoint);
+
+		for (Point point : pointAround) {
+			// Check boundaries
+			if (!this.pointIsOutOfBounds(point)) {
+				squareAround.add(this.getSquare(point));
 			}
 		}
+		return squareAround;
+	}
+
+	private List<Point> getPointAround(Point point) {
+		List<Point> pointAround = new ArrayList<>();
+		pointAround.add(new Point(point.x + 1, point.y + 1));
+		pointAround.add(new Point(point.x - 1, point.y + 1));
+		pointAround.add(new Point(point.x, point.y + 1));
+		pointAround.add(new Point(point.x + 1, point.y));
+		pointAround.add(new Point(point.x - 1, point.y));
+		pointAround.add(new Point(point.x + 1, point.y - 1));
+		pointAround.add(new Point(point.x - 1, point.y - 1));
+		pointAround.add(new Point(point.x, point.y - 1));
+		return pointAround;
+	}
+
+	/**
+	 * Doesn't check boundaries
+	 * 
+	 * @param point
+	 * @return
+	 */
+	public Square getSquare(Point point) {
+		return this.tab[point.y][point.x];
+	}
+
+	/**
+	 * Doesn't check boundaries
+	 * 
+	 * @param point
+	 * @param square
+	 */
+	public void setSquare(Point point, Square square) {
+		this.tab[point.y][point.x] = square;
 	}
 
 	@Override
@@ -92,6 +164,10 @@ public class Board {
 	 */
 	public boolean isMine(int x, int y) {
 		return tab[y][x] instanceof Mine;
+	}
+
+	public boolean isMine(Point point) {
+		return this.getSquare(point) instanceof Mine;
 	}
 
 	public int getHeight() {

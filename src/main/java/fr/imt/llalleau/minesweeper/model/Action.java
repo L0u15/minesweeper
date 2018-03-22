47f6dee17eac377db2725d35d2dd0bf6540fe085
L0u15 <louis.lalleau@gmail.com>
2018-03-22 17:05:19 +0100

    update Action for "redo"

--- src/main/java/fr/imt/llalleau/minesweeper/model/Action.java
+++ src/main/java/fr/imt/llalleau/minesweeper/model/Action.java
@@ -3,8 +3,10 @@ package fr.imt.llalleau.minesweeper.model;
 import java.awt.Point;
 
 public class Action {
-	
+	// Double linked list
+	// For undo & redo
 	private Point point;
+	private Action next;
 	private Action previous;
 
 	public Action(Point point) {
@@ -14,10 +16,19 @@ public class Action {
 	public Action(Point point, Action previous) {
 		this(point);
 		this.previous = previous;
+		this.previous.setNext(this);
 	}
 
 	public Point getPoint() {
 		return point;
+	}
+
+	public Action getNext() {
+		return next;
+	}
+
+	public void setNext(Action next) {
+		this.next = next;
 	}
 
 	public Action getPrevious() {
@@ -27,4 +38,9 @@ public class Action {
 	public boolean isFirst() {
 		return this.previous == null;
 	}
+
+	public boolean isLast() {
+		return this.next == null;
+	}
+
 }
